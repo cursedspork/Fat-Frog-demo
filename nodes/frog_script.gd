b14ba947116ddb25c71d80bca4f0ba3c1d2a76c8
Maddy Walczak <Maciekarp@users.noreply.github.com>
2024-03-27 15:59:39 -0400

    Added empty scenes for water and road and audio players

--- nodes/frog_script.gd
+++ nodes/frog_script.gd
@@ -1,30 +1,43 @@
 extends Node2D
-@export_group("Jump Values")
+
+signal died
+
+@export_group("Jump Variables")
 @export var jump_dist: float = 32 ## Global jump distance in pixels 
 @export var jump_time: float = 1 ## Time to jump in seconds
 @export var jump_curve: Curve ## Animation curve that determines the distance moved over time
 
+@export var jump_audio: AudioStreamPlayer
+
+@export_group("Death Variables")
+@export var death_audio: AudioStreamPlayer
+
+
 var last_time = -1000
-var last_jump_pos: float = 0
+var last_jump_pos: Vector2 = Vector2(0,0)
 var is_waiting_for_jump: bool = false
 var is_jumping: bool = false
 
+var jump_dir: String = "up"
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	last_jump_pos = global_position.y + jump_dist
+	last_jump_pos.y = global_position.y + jump_dist
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	# performs jump movement over time
 	if Time.get_ticks_msec() - 1000 * jump_time < last_time:
-		global_position.y = last_jump_pos - (jump_curve.sample((Time.get_ticks_msec() - last_time)/(1000 * jump_time)) * jump_dist)
+		global_position.y = last_jump_pos.y - (jump_curve.sample((Time.get_ticks_msec() - last_time)/(1000 * jump_time)) * jump_dist)
 	else:
-		global_position.y = last_jump_pos - jump_dist
+		global_position.y = last_jump_pos.y - jump_dist
 		is_jumping = false
 
 func start_jump():
+	jump_audio.play()
 	last_time = Time.get_ticks_msec()
-	last_jump_pos = global_position.y
+	last_jump_pos.y = global_position.y
 	is_jumping = true
 
 func _input(event):
@@ -36,3 +49,7 @@ func _input(event):
 			await get_tree().create_timer((last_time + (1000 * jump_time) - Time.get_ticks_msec())/1000).timeout
 			is_waiting_for_jump = false
 			start_jump()
+
+# when hit die
+func _on_area_2d_area_entered(_area):
+	death_audio.play()
